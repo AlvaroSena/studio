25da3775e06a9cdded3ba924ea1e7b85c288330a
Alvaro Sena <alvarosenacs.c@gmail.com>
2025-09-29 10:24:19 -0300

    chore: add ts, tsx, and express

---
+++ apps/backend/src/app.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+export const app = express();
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  return response.json({ message: "hello, world" });
+});
