c5a1eec2077801a20678dcec8447a1abfccbb159
Alvaro Sena <alvarosenacs.c@gmail.com>
2025-10-12 12:26:51 -0300

    chore: setup rate limit to prevent denial-of-service attacks

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,6 +1,14 @@
 import express from "express";
 import { routes } from "./routes";
+import RateLimit from "express-rate-limit";
 
 export const app = express();
 app.use(express.json());
-app.use(routes)
+
+const limiter = RateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+app.use(routes);
