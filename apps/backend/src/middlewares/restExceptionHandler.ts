c7577c08db3c1f2d582c732fbfea8dae1f2b7a39
Alvaro Sena <alvarosenacs.c@gmail.com>
2025-10-12 14:10:24 -0300

    refactor: optmize exception handler middleware

--- apps/backend/src/middlewares/restExceptionHandler.ts
+++ apps/backend/src/middlewares/restExceptionHandler.ts
@@ -2,23 +2,14 @@ import { Request, Response, NextFunction } from "express";
 import { ConflictException } from "../exceptions/ConflictException";
 import { NotFoundException } from "../exceptions/NotFoundException";
 import { InvalidCredentialsException } from "../exceptions/InvalidCredentialsException";
+import { Exception } from "../exceptions/Exception";
 
 export function restExceptionHandler(err: any, request: Request, response: Response, next: NextFunction) {
   let statusCode = 500;
   let message = "Internal server error";
 
   switch (true) {
-    case err instanceof ConflictException:
-      statusCode = err.statusCode;
-      message = err.message;
-      break;
-
-    case err instanceof NotFoundException:
-      statusCode = err.statusCode;
-      message = err.message;
-      break;
-
-    case err instanceof InvalidCredentialsException:
+    case err instanceof Exception:
       statusCode = err.statusCode;
       message = err.message;
       break;
